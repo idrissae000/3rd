f3249443e2cc69d101b8d3006d2c745fd7ca00c0
idrissae000 <idriss.ae000@gmail.com>
2026-02-16 10:24:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,10 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+module.exports = nextConfig;
